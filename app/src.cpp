394159cac32f7e4be1070914e43462e56f36f35a
gly <glefranc@eig.fr>
2021-07-02 18:32:07 +0200

    WIP on range plus_one example don't work rn

--- app/src.cpp
+++ app/src.cpp
@@ -1,5 +1,11 @@
 #include "rangepp.h"
+#include <array>
+#include <vector>
 int main()
 {
+    auto v = std::array{1,2,3};
+    auto t = v | rpp::plus_one;
+    // auto t = v | std::views::all| std::views::transform([](int i) { return i+1; });
+    auto v2 = std::vector{t.begin(), t.end()};
     return 0;
 }
